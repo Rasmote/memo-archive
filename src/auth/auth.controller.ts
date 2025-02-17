e277c7a4e92b9afb7ef5487895043d7d33129cc2
Rasmote <workrasmote@gmail.com>
2025-02-17 16:37:16 +0900

    123

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
 import { AuthLogInDto, AuthRefreshDto, AuthSignUpDto } from './dto/auth.dto';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('login')
 export class AuthController {
@@ -11,17 +12,56 @@ export class AuthController {
     ) { }
 
     @Post('signup')
+    @ApiOperation({ summary: '회원가입', description: '유저를 회원가입시킴' })
+    @ApiResponse({
+        status: 200,
+        description: `회원가입 성공.`
+    })
+    @ApiResponse({
+        status: 400,
+        description: `"id must be a string","password must be a string"`
+    })
+    @ApiResponse({
+        status: 409,
+        description: `이미 존재하는 ID입니다.`
+    })
     async signup(@Body() body: AuthSignUpDto) {
         return this.usersService.createUser(body);
     }
 
     @Post()
+    @ApiOperation({ summary: '로그인', description: '아이디 패스워드를 받아 로그인 및 토큰발급' })
+    @ApiResponse({
+        status: 200,
+        description: `"access_token": "(tokenValue)","refresh_token": "(tokenValue)"`
+    })
+    @ApiResponse({
+        status: 400,
+        description: `"id must be a string","password must be a string"`
+    })
+    @ApiResponse({
+        status: 401,
+        description: `아이디 혹은 비밀번호가 틀렸습니다.`
+    })
     async login(@Body() body: AuthLogInDto) {
         const user = await this.authservice.checkUser(body);
         return this.authservice.login(user);
     }
 
     @Post('refresh')
+    @ApiOperation({ summary: '엑세스 토큰 재발급', description: '리프레시 토큰을 받아 엑세스 토큰을 재발급' })
+    @ApiResponse({
+        status: 200,
+        description: `"access_token": "(tokenValue)"`
+    })
+    @ApiResponse({
+        status: 400,
+        description: `Rtoken should not be empty`
+    })
+    @ApiResponse({
+        status: 401,
+        description: `리프레시 토큰이 유효하지 않습니다.`
+    })
     async refresh(@Body() body: AuthRefreshDto) {
         return this.authservice.refreshAccessToken(body);
     }
--- src/memo/memo.controller.ts
+++ src/memo/memo.controller.ts
@@ -12,28 +12,33 @@ import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 export class MemoController {
     constructor(private memoService: MemoService) { }
 
+    @Get()
     @ApiOperation({ summary: '모든 메모 조회', description: '해당 유저가 등록한 모든 메모 반환' })
     @ApiResponse({
         status: 200,
         description: `[{"num": number,"title": string,}, ...]`
     })
-    @Get()
     async getAllMemos(@Req() req: Request) {
         const user = req.user as UserEntity;
         return this.memoService.getAllMemos(user);
     }
 
+    @Post('create')
     @ApiOperation({ summary: '메모 작성', description: '메모를 추가함' })
     @ApiResponse({
         status: 200,
         description: `메모가 생성되었습니다.`
     })
-    @Post('create')
+    @ApiResponse({
+        status: 400,
+        description: `"title must be a string","content must be a string"`
+    })
     async createMemo(@Body() dto: CreateMemoDto, @Req() req: Request) {
         const user = req.user as UserEntity;
         return this.memoService.createMemo(dto, user);
     }
 
+    @Get('read/:num')
     @ApiOperation({ summary: '특정 메모 조회', description: '메모를 읽어옴' })
     @ApiResponse({
         status: 200,
@@ -43,29 +48,40 @@ export class MemoController {
         status: 404,
         description: `해당 메모를 찾을 수 없습니다.`
     })
-    @Get('read/:num')
     async readMemo(@Param('num') num: number, @Req() req: Request) {
         const user = req.user as UserEntity;
         return this.memoService.readMemo(num, user);
     }
 
+    @Post('update')
     @ApiOperation({ summary: '메모 수정', description: '메모를 수정함' })
     @ApiResponse({
         status: 200,
         description: `메모가 수정되었습니다.`
     })
-    @Post('update')
+    @ApiResponse({
+        status: 400,
+        description: `"num must be a number conforming to the specified constraints","title must be a string""content must be a string"`
+    })
+    @ApiResponse({
+        status: 404,
+        description: `해당 메모를 찾을 수 없습니다.`
+    })
     async updateMemo(@Body() dto: UpdateMemoDto, @Req() req: Request) {
         const user = req.user as UserEntity;
         return this.memoService.updateMemo(dto, user);
     }
 
+    @Get('delete/:num')
     @ApiOperation({ summary: '메모 삭제', description: '특정 메모를 삭제함' })
     @ApiResponse({
         status: 200,
         description: `메모가 삭제되었습니다.`
     })
-    @Get('delete/:num')
+    @ApiResponse({
+        status: 404,
+        description: `해당 메모를 찾을 수 없습니다.`
+    })
     async deleteMemo(@Param('num') num: number, @Req() req: Request) {
         const user = req.user as UserEntity;
         return this.memoService.deleteMemo(num, user);
